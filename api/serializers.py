35db2224fb0214cc47479f4249a7ed5eef2bee91
Victor Castaneda <vidanilo10@gmail.com>
2025-04-13 17:32:24 -0500

    Add serializers

---
+++ api/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from api.models import Customer, Loan, Payment, PaymentDetail
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+
+
+class LoanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Loan
+        fields = '__all__'
+
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
+
+
+class PaymentDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PaymentDetail
+        fields = '__all__'
