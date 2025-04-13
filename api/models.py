8b0009281bb0b052c77f995c2273d1f5cc245780
Victor Castaneda <vidanilo10@gmail.com>
2025-04-13 17:00:03 -0500

    Improve models

--- api/models.py
+++ api/models.py
@@ -1,22 +1,32 @@
 from django.db import models
 
-# Create your models here.
-
 class Customer(models.Model):
     created_timestamp = models.DateTimeField()
     updated_timestamp = models.DateTimeField()
     external_id_character = models.IntegerField()
-    status = models.CharField(max_length=10)
-    score = models.IntegerField()
+    score = models.DecimalField()
     preapproved_at = models.DateTimeField()
+
+    class CustomerStatus(models.IntegerChoices):
+        ACTIVE = 1, "Active"
+        INACTIVE = 2, "Inactive"
+
+    status = models.IntegerField(
+        choices=CustomerStatus.choices,
+        default=CustomerStatus.ACTIVE,
+        verbose_name="Estado"
+    )
+
+    def __str__(self):
+        return self.status
 
 
 class Loan(models.Model):
     created_timestamp = models.DateTimeField()
     updated_timestamp = models.DateTimeField()
     external_id_character = models.IntegerField()
-    amount = models.IntegerField()
-    status = models.CharField(max_length=10)
+    amount = models.DecimalField()
+    
     contract_version_character = models.CharField(max_length=30)
     maximum_payment_date = models.DateTimeField()
     taken_at = models.DateTimeField()
@@ -24,11 +34,53 @@ class Loan(models.Model):
     outstanding = models.IntegerField()
 
 
+    class LoanStatus(models.IntegerChoices):
+        PENDING = 1, "Active"
+        ACTIVE = 2, "Inactive"
+        REJECTED = 3, "Rejected"
+        PAID = 4, "Paid"
+
+    status = models.IntegerField(
+        choices=LoanStatus.choices,
+        default=LoanStatus.PENDING,
+        verbose_name="Estado"
+    )
+
+
+    def __str__(self):
+        return self.contract_version_character
+
+
+
+
 class Payment(models.Model):
     created_timestamp = models.DateTimeField()
     updated_timestamp = models.DateTimeField()
-    external_id_character = models.IntegerField()
-    total_amount = models.IntegerField()
-    status = models.CharField(max_length=10)
+    external_id_character = models.CharField(max_length=60)
+    total_amount = models.DecimalField()
     paid_at = models.DateTimeField()
     customer_id = models.IntegerField()
+
+
+    class PaymentStatus(models.IntegerChoices):
+        COMPLETED = 1, "Completed"
+        REJECTED = 2, "Rejected"
+
+    status = models.IntegerField(
+        choices=PaymentStatus.choices,
+        verbose_name="Estado"
+    )
+
+
+    def __str__(self):
+        return self.status
+
+
+
+class PaymentDetail(models.Model):
+    created_timestamp = models.DateTimeField()
+    updated_timestamp = models.DateTimeField()
+    amount = models.DecimalField()
+    loan_id = models.IntegerField()
+    payment_id = models.IntegerField()
+
