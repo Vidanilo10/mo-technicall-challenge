b6ac8cb78945d90c27cfb37c253be74400627290
Victor Castaneda <vidanilo10@gmail.com>
2025-04-15 11:43:07 -0500

    Improve customer tests

--- tests/test_customers.py
+++ tests/test_customers.py
@@ -8,6 +8,20 @@ from django.apps import apps
 from rest_framework.test import APIClient
 from rest_framework.test import RequestsClient
 
+@pytest.fixture
+def get_url():
+    return "http://localhost:8000/"
+
+
+@pytest.fixture
+def get_customer_body():
+    return {
+        "external_id_character": "external_03",
+        "score": 6000.0,
+        "preapproved_at": "2023-02-12T22:29:27.177914Z",
+        "status": 1
+    }
+
 
 @pytest.fixture
 def create_superuser():
@@ -22,22 +36,18 @@ def get_client():
 
 
 @pytest.fixture
-def get_token(get_client):
-    response = get_client.post("http://localhost:8000/customers-token-auth/", data={"username": "demo", "password": "demo"})
+def get_token(get_client, get_url):
+    response = get_client.post(f"{get_url}customers-token-auth/", data={"username": "demo", "password": "demo"})
     return json.loads(response.content).get("token")
 
 
 @pytest.fixture
-def create_customer(create_superuser, get_client, get_token):
+def create_customer(create_superuser, get_client, get_token, get_url, get_customer_body):
+    create_superuser
     client = get_client
     response = client.post(
-        "http://localhost:8000/api/v1/customer/", 
-        data={
-            "external_id_character": "external_03",
-            "score": 6000.0,
-            "preapproved_at": "2023-02-12T22:29:27.177914Z",
-            "status": 1
-        }, 
+        f"{get_url}api/v1/customer/", 
+        data=get_customer_body, 
         headers={"Authorization": f"Token {get_token}"}
     )
     
@@ -46,23 +56,20 @@ def create_customer(create_superuser, get_client, get_token):
 
 
 @pytest.mark.django_db
-def test_list_customers(create_superuser, get_client, get_token):
+def test_list_customers(create_superuser, get_client, get_token, get_url):
+    create_superuser
     client = get_client
-    response = client.get("http://localhost:8000/api/v1/customer/", headers={"Authorization": f"Token {get_token}"})
+    response = client.get(f"{get_url}api/v1/customer/", headers={"Authorization": f"Token {get_token}"})
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test_create_customer(create_superuser, get_client, get_token):
+def test_create_customer(create_superuser, get_client, get_token, get_url, get_customer_body):
+    create_superuser
     client = get_client
     response = client.post(
-        "http://localhost:8000/api/v1/customer/", 
-        data={
-            "external_id_character": "external_03",
-            "score": 6000.0,
-            "preapproved_at": "2023-02-12T22:29:27.177914Z",
-            "status": 1
-        }, 
+        f"{get_url}api/v1/customer/", 
+        data=get_customer_body,
         headers={"Authorization": f"Token {get_token}"}
     )
     
@@ -73,8 +80,9 @@ def test_create_customer(create_superuser, get_client, get_token):
 
 
 @pytest.mark.django_db
-def test_get_customer_balance(create_superuser, get_client, get_token, create_customer):
+def test_get_customer_balance(create_superuser, get_client, get_token, create_customer, get_url):
+    create_superuser
     customer_external_id = create_customer.get("external_id_character")
     client = get_client
-    response = client.get(f"http://localhost:8000/api/v1/customer/{customer_external_id}/get_customer_balance/", headers={"Authorization": f"Token {get_token}"})
+    response = client.get(f"{get_url}api/v1/customer/{customer_external_id}/get_customer_balance/", headers={"Authorization": f"Token {get_token}"})
     assert response.status_code == 200
